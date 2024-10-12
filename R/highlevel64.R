e6b93c252d9a67a3ecfb737efa665982664ebd54
Michael Chirico <michaelchirico4@gmail.com>
2024-10-11 22:52:49 -0700

    ?tiepos

--- R/highlevel64.R
+++ R/highlevel64.R
@@ -2406,48 +2406,39 @@ keypos.integer64 <- function(x
   p
 }
 
-#' \name{tiepos}
-#' \alias{tiepos}
-#' \alias{tiepos.integer64}
-#' \title{Extract Positions of Tied Elements}
-#' \description{
-#'   `tiepos` returns the positions of those elements that participate in ties.
-#' }
-#' \usage{
-#' tiepos(x, ...)
-#' \method{tiepos}{integer64}(x, nties = NULL, method = NULL, ...)
-#' }
-#' \arguments{
-#'   \item{x}{a vector or a data frame or an array or `NULL`.}
-#'   \item{nties}{NULL or the number of tied values (including NA). Providing `nties` can speed-up when `x` has no cache. Note that a wrong nties can cause undefined behaviour up to a crash.}
-#'   \item{method}{NULL for automatic method selection or a suitable low-level method, see details}
-#'   \item{...}{ignored}
-#' }
-#' \details{
-#'   This function automatically chooses from several low-level functions considering the size of `x` and the availability of a cache.
-#'   Suitable methods are [sortordertie()] (fast ordering)
-#' and [ordertie()] (memory saving ordering).
-#' }
-#' \value{
-#'   an integer vector of positions
-#' }
-#' \author{
-#'     Jens Oehlschlägel <Jens.Oehlschlaegel@truecluster.com>
-#' }
-#' \seealso{
-#'   [rank.integer64()] for possibly tied ranks and [unipos.integer64()] for positions of unique values.
-#' }
-#' \examples{
+#' Extract Positions of Tied Elements
+#'
+#' `tiepos` returns the positions of those elements that participate in ties.
+#'
+#' @param x a vector or a data frame or an array or `NULL`.
+#' @param nties NULL or the number of tied values (including NA). Providing
+#'   `nties` can speed-up when `x` has no cache. Note that a wrong nties can
+#'   cause undefined behaviour up to a crash.
+#' @param method NULL for automatic method selection or a suitable low-level
+#'   method, see details
+#' @param ... ignored
+#'
+#' @details
+#' This function automatically chooses from several low-level functions
+#'   considering the size of `x` and the availability of a cache.
+#'
+#' Suitable methods are
+#'  - [sortordertie] (fast ordering)
+#'  - [ordertie] (memory saving ordering).
+#'
+#' @return an integer vector of positions
+#' @seealso [rank.integer64()] for possibly tied ranks and [unipos.integer64()]
+#'   for positions of unique values.
+#'
+#' @examples
 #' x <- as.integer64(sample(c(rep(NA, 9), 1:9), 32, TRUE))
 #' tiepos(x)
 #'
 #' stopifnot(identical(tiepos(x),  (1:length(x))[duplicated(x) | rev(duplicated(rev(x)))]))
-#' }
-#' \keyword{manip}
-#' \keyword{univar}
-
-
+#' @keywords manip univar
+#' @export
 tiepos <- function(x, ...)UseMethod("tiepos")
+#' @export
 tiepos.integer64 <- function(x
 , nties = NULL
 , method = NULL
