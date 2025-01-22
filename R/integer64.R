9154b33659cc4274886cb636de5934f2e7dcdc50
Michael Chirico <michaelchirico4@gmail.com>
2025-01-22 13:05:18 -0800

    update header

--- R/integer64.R
+++ R/integer64.R
@@ -1,11 +1,11 @@
 # /*
 # R-Code
 # S3 atomic 64bit integers for R
-# (c) 2011 Jens Oehlschägel
+# (c) 2011-2024 Jens Oehlschägel
+# (c) 2025 Michael Chirico
 # Licence: GPL2
 # Provided 'as is', use at your own risk
 # Created: 2011-12-11
-# Last changed:  2011-12-11
 #*/
 
 #' Identity function for class 'integer64'
