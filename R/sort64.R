2bd908e76a27e7d1fac877ceb3bc266bdcd9cd1b
Michael Chirico <michaelchirico4@gmail.com>
2025-01-24 23:07:20 -0800

    Move bit to Imports

--- R/sort64.R
+++ R/sort64.R
@@ -54,21 +54,21 @@
 #'   x <- as.integer64(sample(c(rep(NA, 9), 1:9), 32, TRUE))
 #'   x
 #'   message("ramsort example")
-#'   s <- clone(x)
-#'   ramsort(s)
+#'   s <- bit::clone(x)
+#'   bit::ramsort(s)
 #'   message("s has been changed in-place - whether or not ramsort uses an in-place algorithm")
 #'   s
 #'   message("ramorder example")
-#'   s <- clone(x)
+#'   s <- bit::clone(x)
 #'   o <- seq_along(s)
-#'   ramorder(s, o)
+#'   bit::ramorder(s, o)
 #'   message("o has been changed in-place - s remains unchanged")
 #'   s
 #'   o
 #'   s[o]
 #'   message("ramsortorder example")
 #'   o <- seq_along(s)
-#'   ramsortorder(s, o)
+#'   bit::ramsortorder(s, o)
 #'   message("s and o have both been changed in-place - this is much faster")
 #'   s
 #'   o
