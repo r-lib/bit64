6d414c2cc7433e9721a0939ffcf8953ea5f5e962
Michael Chirico <michaelchirico4@gmail.com>
2026-01-01 15:12:15 -0800

    ws style

--- tests/testthat/test-zzz.R
+++ tests/testthat/test-zzz.R
@@ -2,7 +2,7 @@ test_that("substitute() invocation of generic doesn't warn during transition", {
   x = as.integer64(1:10)
   expect_no_warning(
     expect_identical(
-      eval(substitute(f(x, y), list(f = `:`, x = x[1L], y = x[10L]))),
+      eval(substitute(f(x, y), list(f=`:`, x=x[1L], y=x[10L]))),
       x
     )
   )
