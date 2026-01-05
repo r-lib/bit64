cb4d2d150ae2cbe586af3b7a2d5f48dc24438a7e
hcirellu <christian.ullerich@web.de>
2026-01-05 16:43:10 +0100

    introduce helpful call information in warning and error messages like in PR (#209)

--- R/integer64.R
+++ R/integer64.R
@@ -822,6 +822,40 @@ str.integer64 <- function(object,
   invisible()
 }
 
+# TODO(#59): With the PR(#209) to add a generic for table() a similar function is used. This here is better so that it shall replace the other.
+choose_sys_call = function(function_names, name_to_display=NULL) {
+  sc = sys.calls()
+  sc_length = length(sc)
+  if (sc_length == 1L || length(function_names) == 0L) return(sc[[1L]])
+  sc_char = vapply(sc, function(el) if (is.function(el[[1L]])) "" else rev(as.character(el[[1L]]))[1L], "")
+  sel = rev(which(sc_char == function_names[length(function_names)]))[1L]
+  if (is.na(sel)) 
+    sel = 1L
+  for (i in rev(seq_along(function_names))[-1L]) {
+    if (sel == 1L || sc_char[sel - 1L] != function_names[i])
+      break
+    sel = sel - 1L
+  }
+  sc = sc[[sel]]
+  if (!is.null(name_to_display))
+    sc[[1L]] = as.name(name_to_display)
+  sc
+}
+withCallingHandlers_and_choose_call = function(expr, function_names, name_to_display=NULL) {
+  wch = str2lang("withCallingHandlers(expr, error=error, warning=warning)")
+  wch[[2L]] = sys.call()[[2L]] # expr
+  wch[[3L]] = {function(function_names, name_to_display) 
+    function(e) {stop(errorCondition(e$message, call=choose_sys_call(function_names, name_to_display)))}
+  }(function_names, name_to_display)
+  wch[[4L]] = {function(function_names, name_to_display) 
+    function(w) {
+      warning(warningCondition(w$message, call=choose_sys_call(function_names, name_to_display)))
+      invokeRestart("muffleWarning")
+    }
+  }(function_names, name_to_display)
+  eval(wch, envir=parent.frame())
+}
+
 #' @rdname extract.replace.integer64
 #' @export
 `[.integer64` = function(x, i, j, ..., drop=TRUE) {
@@ -835,7 +869,7 @@ str.integer64 <- function(object,
   args$drop = FALSE
   if (length(args) == 1L) return(x)
   oldClass(x) = NULL
-  ret = do.call("[", c(list(x=x), args))
+  withCallingHandlers_and_choose_call({ret = do.call("[", c(list(x=x), args))}, c("[", "[.integer64"))  
   NA_integer64_real = NA_integer64_
   oldClass(NA_integer64_real) = NULL
 
@@ -898,11 +932,11 @@ str.integer64 <- function(object,
   if (is.character(value) || is.complex(value) || (is.double(value) && class(value)[1L] != "numeric")) {
     args$value = value
     x = structure(as(x, class(value)[1L]), dim = dim(x), dimnames = dimnames(x))
-    ret = do.call("[<-", c(list(x=x), args))
+    withCallingHandlers_and_choose_call({ret = do.call("[<-", c(list(x=x), args))}, c("[<-", "[<-.integer64"))  
   } else {
     args$value = as.integer64(value)
     oldClass(x) = NULL
-    ret = do.call("[<-", c(list(x=x), args))
+    withCallingHandlers_and_choose_call({ret = do.call("[<-", c(list(x=x), args))}, c("[<-", "[<-.integer64"))  
     oldClass(ret) = "integer64"
   }
   ret
@@ -917,7 +951,7 @@ str.integer64 <- function(object,
     el
   }})
   oldClass(x) = NULL
-  ret = do.call("[[", c(list(x=x), args))
+  withCallingHandlers_and_choose_call({ret = do.call("[[", c(list(x=x), args))}, c("[[", "[[.integer64"))  
   oldClass(ret) = "integer64"
   ret
 }
@@ -933,11 +967,11 @@ str.integer64 <- function(object,
   if (is.character(value) || is.complex(value) || (is.double(value) && class(value)[1L] != "numeric")) {
     args$value = value
     x = structure(as(x, class(value)[1L]), dim = dim(x), dimnames = dimnames(x))
-    ret = do.call("[[<-", c(list(x=x), args))
+    withCallingHandlers_and_choose_call({ret = do.call("[[<-", c(list(x=x), args))}, c("[[<-", "[[<-.integer64"))  
   } else {
-    value = as.integer64(value)
+    args$value = as.integer64(value)
     oldClass(x) = NULL
-    ret = do.call("[[<-", c(list(x=x), args, list(value=value)))
+    withCallingHandlers_and_choose_call({ret = do.call("[[<-", c(list(x=x), args))}, c("[[<-", "[[<-.integer64"))  
     oldClass(ret) = "integer64"
   }
   ret
