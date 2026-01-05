ebd03dbc2aa859832fc9b6f935a202a8ca93a12b
hcirellu <christian.ullerich@web.de>
2026-01-05 17:07:55 +0100

    backport `str2lang()`, `errorCondition()` and `warningCondition()`

--- R/zzz.R
+++ R/zzz.R
@@ -8,6 +8,27 @@
 # */
 
 # nocov start
+.onLoad = function(libname, pkgname) {
+  
+  ns = parent.env(environment())
+
+  if (getRversion() < "3.6.0") {
+    # backport condition constructors from R 3.6.0
+    errorCondition = function(message, ..., class = NULL, call = NULL) 
+      structure(list(message = as.character(message), call = call, ...), class = c(class, "error", "condition"))
+    assign("errorCondition", errorCondition, envir = ns, inherits = FALSE)
+  
+    warningCondition = function(message, ..., class = NULL, call = NULL) 
+      structure(list(message = as.character(message), call = call, ...), class = c(class, "warning", "condition"))
+    assign("warningCondition", warningCondition, envir = ns, inherits = FALSE)
+    
+    str2lang = function(s) 
+      parse(text = s, keep.source=FALSE)[[1L]]
+    assign("str2lang", str2lang, envir = ns, inherits = FALSE)
+  }
+  
+}
+
 .onUnload <- function(libpath) {
   library.dynam.unload("bit64", libpath)
 }
