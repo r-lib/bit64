828ca3ce197fbdc7f022cac6dbfab230065b4bc4
hcirellu <christian.ullerich@web.de>
2026-01-05 09:02:05 +0100

    small fix for ignore_attr

--- tests/testthat/test-matrix64.R
+++ tests/testthat/test-matrix64.R
@@ -241,9 +241,9 @@ test_that("coercion to matrix and array", {
   i64 = as.integer64(i32)
   m64 = matrix(as.integer64(i32), 2L)
 
-  expect_identical(as.matrix(i64), structure(i64, dim = c(length(i64), 1L)), ignore_attr=if (getRversion() < "4.0.0") "class")
+  expect_identical(as.matrix(i64), structure(i64, dim = c(length(i64), 1L)), ignore_attr=if (getRversion() < "4.0.0") "class" else FALSE)
   expect_identical(as.matrix(m64), m64)
 
-  expect_identical(as.array(i64), structure(i64, dim = c(length(i64))), ignore_attr=if (getRversion() < "4.0.0") "class")
+  expect_identical(as.array(i64), structure(i64, dim = c(length(i64))), ignore_attr=if (getRversion() < "4.0.0") "class" else FALSE)
   expect_identical(as.array(m64), m64)
 })
