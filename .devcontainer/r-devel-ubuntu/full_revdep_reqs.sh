eace19906cca26903091cc34b8634e8045f457fd
Michael Chirico <chiricom@google.com>
2026-01-06 23:35:15 +0000

    new requirements

--- .devcontainer/r-devel-ubuntu/full_revdep_reqs.sh
+++ .devcontainer/r-devel-ubuntu/full_revdep_reqs.sh
@@ -8,5 +8,7 @@
 # Also recommend adding -Wno-ignored-attributes to ~/.R/Makevars to reduce RcppEigen logs dump
 
 # As of 2026-01-02, the following system requirements are needed, too:
+apt-get update
+apt install cmake
 apt-get install -y --no-install-recommends \
-  libxml2-dev
+  libxml2-dev libcurl4-openssl-dev libssl-dev libv8-dev
