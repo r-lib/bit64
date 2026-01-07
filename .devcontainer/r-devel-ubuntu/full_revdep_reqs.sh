b6ef98f95d36e2973b68cca2dba0b984b6ef08e2
Michael Chirico <chiricom@google.com>
2026-01-07 06:34:44 +0000

    in comes bioconductor

--- .devcontainer/r-devel-ubuntu/full_revdep_reqs.sh
+++ .devcontainer/r-devel-ubuntu/full_revdep_reqs.sh
@@ -1,9 +1,21 @@
 # Find all tested revdeps as follows:
-# db = available.packages()
-# all_revdeps = union(
-#   tools::dependsOnPkgs("bit64", installed=db),
-#   tools::dependsOnPkgs("bit64", dependencies="all", recursive=FALSE, installed=db)
+#
+# install.packages("BiocManager")
+#
+# bioc_db = available.packages(repos = BiocManager::repositories())
+# bioc_revdeps = union(
+#   tools::dependsOnPkgs("bit64", installed=bioc_db),
+#   tools::dependsOnPkgs("bit64", dependencies="all", recursive=FALSE, installed=bioc_db)
 # )
+#
+# cran_db = available.packages()
+# cran_revdeps = union(
+#   tools::dependsOnPkgs("bit64", installed=cran_db),
+#   tools::dependsOnPkgs("bit64", dependencies="all", recursive=FALSE, installed=cran_db)
+# )
+#
+# bioc_only_revdeps = bioc_revdeps[grep("bioconductor", bioc_db[bioc_revdeps, "Repository"])]
+# cran_only_revdeps = setdiff(cran_revdeps, bioc_revdeps)
 
 # Also recommend adding -Wno-ignored-attributes to ~/.R/Makevars to reduce RcppEigen logs dump
 
@@ -13,4 +25,6 @@ apt install cmake
 apt-get install -y --no-install-recommends \
   libxml2-dev libcurl4-openssl-dev libssl-dev libv8-dev \
   libfontconfig1-dev libharfbuzz-dev libfribidi-dev libcairo2-dev \
-  libudunits2-dev libgdal-dev libabsl-dev
+  libudunits2-dev libgdal-dev libabsl-dev \
+  libmpfr-dev libglpk-dev libssh-dev libmagick++-dev default-jdk \
+  libsasl2-dev tcl-dev tk-dev libgsl-dev
