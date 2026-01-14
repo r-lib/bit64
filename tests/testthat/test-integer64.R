844fe8f2790d5d24e1af765ff5026f374054bba8
hcirellu <christian.ullerich@web.de>
2026-01-14 16:59:33 +0100

    fix ancient test

--- tests/testthat/test-integer64.R
+++ tests/testthat/test-integer64.R
@@ -869,8 +869,8 @@ with_parameters_test_that("intersect works with basic R types (except double)", 
         expected_result_y_x = base::intersect(y, x)
       } else {
         expected_result_x_y = base::intersect(x, y)
-        if (getRversion() <= "3.6.0" && identical(x, character()) && type_y == "integer64")
-          expected_result_x_y = character()
+        if (getRversion() <= "3.6.0" && type_x == "character" && type_y == "integer64")
+          expected_result_x_y = as.character(expected_result_x_y)
         expected_result_y_x = base::intersect(y, x)
       }
       y = as(y, type_y)
