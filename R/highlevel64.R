fef30a68294c7dc07b0dc6646e1bf15e3960f3b6
hcirellu <christian.ullerich@web.de>
2026-01-15 09:03:12 +0100

    extend comment

--- R/highlevel64.R
+++ R/highlevel64.R
@@ -1965,8 +1965,9 @@ unipos.integer64 = function(x,
 #' @param deparse.level controls how the default `dnn` is constructed. See Details.
 #'
 #' @details
-#' If one argument of ... is `integer64` the `table.integer64` method is used. Only this 
-#' method supports the arguments `return`, `order`, `nunique`, and `method`.
+#' If at least one argument of ... is integer64 and the remaining arguments of ... 
+#' are integer64 or integer the `table.integer64` method is used. Only this method 
+#' supports the arguments `return`, `order`, `nunique`, and `method`.
 #' 
 #' This function automatically chooses from several low-level functions considering
 #'   the size of `x` and the availability of a cache.
