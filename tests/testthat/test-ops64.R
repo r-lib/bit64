25a1aa6d2db4a8db7f005ebe5aa51404784105d2
hcirellu <christian.ullerich@web.de>
2026-01-19 10:51:17 +0100

    todo #248

--- tests/testthat/test-ops64.R
+++ tests/testthat/test-ops64.R
@@ -182,7 +182,7 @@ test_that("Comparison operators", {
 
 with_parameters_test_that("ops with different classes in combination with integer64 (returning integer64):", local({
 
-  # TODO: uncomment when fixed: as.integer64(-10L)%%7L = -3L vs. as.integer(-10L)%%7L = 4L
+  # TODO(#248): uncomment when fixed: as.integer64(-10L)%%7L = -3L vs. as.integer(-10L)%%7L = 4L
   # x32 = c(-10:-1, 1:10)
   x32 = 1:10
   x64 = as.integer64(x32)
@@ -211,7 +211,7 @@ with_parameters_test_that("ops with different classes in combination with intege
 with_parameters_test_that("ops with different classes in combination with integer64 (not returning integer64):", local({
 
   if (getRversion() >= "4.3.0") {
-    # TODO: uncomment when fixed: as.integer64(-10L)%%7L = -3L vs. as.integer(-10L)%%7L = 4L
+    # TODO(#248): uncomment when fixed: as.integer64(-10L)%%7L = -3L vs. as.integer(-10L)%%7L = 4L
     # x32 = c(-10:-1, 1:10)
     x32 = 1:10
     x64 = as.integer64(x32)
