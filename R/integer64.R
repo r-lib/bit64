0f3d32a57394b80252e816f469fa687af9244f17
Michael Chirico <chiricom@google.com>
2026-01-20 09:56:29 -0800

    update year in integer64.R

--- R/integer64.R
+++ R/integer64.R
@@ -2,7 +2,7 @@
 # R-Code
 # S3 atomic 64bit integers for R
 # (c) 2011-2024 Jens Oehlschägel
-# (c) 2025 Michael Chirico
+# (c) 2025-2026 Michael Chirico
 # Licence: GPL2
 # Provided 'as is', use at your own risk
 # Created: 2011-12-11
