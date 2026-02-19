d590be4363a69a8556388dce6430814676b52304
Michael Chirico <michaelchirico4@gmail.com>
2026-02-19 15:06:45 +0800

    more style

--- tests/testthat/test-setops64.R
+++ tests/testthat/test-setops64.R
@@ -19,15 +19,9 @@ with_parameters_test_that(
     }
     y = as(y, type_y)
 
-    expect_identical(
-      union(x, y),
-      expected_result_x_y
-    )
-    expect_identical(
-      union(y, x),
-      expected_result_y_x
-    )
-  }, 
+    expect_identical(union(x, y), expected_result_x_y)
+    expect_identical(union(y, x), expected_result_y_x)
+  },
   .cases=expand.grid(
     x=I(list(NULL, c(1:7, 2L, 11L))),
     type_x=c(
@@ -40,23 +34,38 @@ with_parameters_test_that(
 )
 
 test_that("union works with special logic for double", {
-  
-  expect_identical(union(1.5:7, 5:10), base::union(1.5:7, 5:10))
-  expect_identical(union(1.5:7, as.integer64(5:10)), as.integer64(1:10))
-  expect_identical(union(as.integer64(5:10), 1.5:7), as.integer64(c(5:10, 1:4)))
-  
+
+  expect_identical(
+    union(1.5:7, 5:10),
+    base::union(1.5:7, 5:10)
+  )
+  expect_identical(
+    union(1.5:7, as.integer64(5:10)),
+    as.integer64(1:10)
+  )
+  expect_identical(
+    union(as.integer64(5:10), 1.5:7),
+    as.integer64(c(5:10, 1:4))
+  )
+
 })
 
 test_that("union works (additional cases)", {
-  
-  expect_identical(union(c(5, 3, 5), c(0, 2, 1, 3, 6)), base::union(c(5, 3, 5), c(0, 2, 1, 3, 6)))
-  expect_identical(union(c(5L, 3L, 5L), as.integer64(c(0, 2, 1, 3, 6))), as.integer64(c(5, 3, 0, 2, 1, 6)))
-  
+
+  expect_identical(
+    union(c(5, 3, 5), c(0, 2, 1, 3, 6)),
+    base::union(c(5, 3, 5), c(0, 2, 1, 3, 6))
+  )
+  expect_identical(
+    union(c(5L, 3L, 5L), as.integer64(c(0, 2, 1, 3, 6))),
+    as.integer64(c(5, 3, 0, 2, 1, 6))
+  )
+
   expect_identical(union(NA, NA_integer_), base::union(NA, NA_integer_))
   expect_identical(union(NA_integer_, NA), base::union(NA_integer_, NA))
   expect_identical(union(NA, NA_integer64_), NA_integer64_)
   expect_identical(union(NA_integer64_, NA), NA_integer64_)
-  
+
 })
 
 
@@ -85,15 +94,9 @@ with_parameters_test_that(
     }
     y = as(y, type_y)
 
-    expect_identical(
-      intersect(x, y),
-      expected_result_x_y
-    )
-    expect_identical(
-      intersect(y, x),
-      expected_result_y_x
-    )
-  }, 
+    expect_identical(intersect(x, y), expected_result_x_y)
+    expect_identical(intersect(y, x), expected_result_y_x)
+  },
   .cases=expand.grid(
     x=I(list(NULL, c(1:7, 2L, 11L))),
     type_x=c(
@@ -106,23 +109,29 @@ with_parameters_test_that(
 )
 
 test_that("intersect works with special logic for double", {
-  
+
   expect_identical(intersect(1.5:7, 5:10), base::intersect(1.5:7, 5:10))
   expect_identical(intersect(1.5:7, as.integer64(5:10)), as.integer64(5:6))
   expect_identical(intersect(as.integer64(5:10), 1.5:7), as.integer64(5:6))
-  
+
 })
 
 test_that("intersect works (additional cases)", {
-  
-  expect_identical(intersect(c(5, 3, 5), c(0, 2, 1, 3, 6)), base::intersect(c(5, 3, 5), c(0, 2, 1, 3, 6)))
-  expect_identical(intersect(as.integer64(c(5, 3, 5)), as.integer64(c(0, 2, 1, 3, 6))), as.integer64(3L))
-  
+
+  expect_identical(
+    intersect(c(5, 3, 5), c(0, 2, 1, 3, 6)),
+    base::intersect(c(5, 3, 5), c(0, 2, 1, 3, 6))
+  )
+  expect_identical(
+    intersect(as.integer64(c(5, 3, 5)), as.integer64(c(0, 2, 1, 3, 6))),
+    as.integer64(3L)
+  )
+
   expect_identical(intersect(NA, NA_integer_), base::intersect(NA, NA_integer_))
   expect_identical(intersect(NA_integer_, NA), base::intersect(NA_integer_, NA))
   expect_identical(intersect(NA, NA_integer64_), NA_integer64_)
   expect_identical(intersect(NA_integer64_, NA), NA_integer64_)
-  
+
 })
 
 
@@ -141,15 +150,9 @@ with_parameters_test_that(
     }
     y = as(y, type_y)
 
-    expect_identical(
-      setdiff(x, y),
-      expected_result_x_y
-    )
-    expect_identical(
-      setdiff(y, x),
-      expected_result_y_x
-    )
-  }, 
+    expect_identical(setdiff(x, y), expected_result_x_y)
+    expect_identical(setdiff(y, x), expected_result_y_x)
+  },
   .cases=expand.grid(
     x=I(list(NULL, c(1:7, 2L, 11L))),
     type_x=c(
@@ -162,23 +165,28 @@ with_parameters_test_that(
 )
 
 test_that("setdiff works with special logic for double", {
-  
+
   expect_identical(setdiff(1.5:7, 5:10), base::setdiff(1.5:7, 5:10))
   expect_identical(setdiff(1.5:7, as.integer64(5:10)), 1.5:4.5)
   expect_identical(setdiff(as.integer64(5:10), 1.5:7), as.integer64(7:10))
-  
+
 })
 
 test_that("setdiff works (additional cases)", {
-  
-  expect_identical(setdiff(c(5, 3, 5), c(0, 2, 1, 3, 6)), base::setdiff(c(5, 3, 5), c(0, 2, 1, 3, 6)))
-  expect_identical(setdiff(as.integer64(c(5, 3, 5)), as.integer64(c(0, 2, 1, 3, 6))), as.integer64(5L))
-  
+
+  expect_identical(
+    setdiff(c(5, 3, 5), c(0, 2, 1, 3, 6)),
+    base::setdiff(c(5, 3, 5), c(0, 2, 1, 3, 6)))
+  expect_identical(
+    setdiff(as.integer64(c(5, 3, 5)), as.integer64(c(0, 2, 1, 3, 6))),
+    as.integer64(5L)
+  )
+
   expect_identical(setdiff(NA, NA_integer_), base::setdiff(NA, NA_integer_))
   expect_identical(setdiff(NA_integer_, NA), base::setdiff(NA_integer_, NA))
   expect_identical(setdiff(NA, NA_integer64_), logical())
   expect_identical(setdiff(NA_integer64_, NA), integer64())
-  
+
 })
 
 with_parameters_test_that(
@@ -191,15 +199,9 @@ with_parameters_test_that(
     expected_result_y_x = base::setequal(y, x)
     y = as(y, type_y)
 
-    expect_identical(
-      setequal(x, y),
-      expected_result_x_y
-    )
-    expect_identical(
-      setequal(y, x),
-      expected_result_y_x
-    )
-  }, 
+    expect_identical(setequal(x, y), expected_result_x_y)
+    expect_identical(setequal(y, x), expected_result_y_x)
+  },
   .cases=expand.grid(
     x=I(list(NULL, c(1:7, 2L, 11L))),
     type_x=c(
@@ -212,7 +214,7 @@ with_parameters_test_that(
 )
 
 test_that("setequal works with special logic for double", {
-  
+
   expect_identical(setequal(1.5:7, 5:10), base::setequal(1.5:7, 5:10))
   expect_true(setequal(1.5:7, as.integer64(6:1)))
   expect_true(setequal(as.integer64(6:1), 1.5:7))
@@ -220,16 +222,25 @@ test_that("setequal works with special logic for double", {
 })
 
 test_that("setequal works (additional cases)", {
-  
-  expect_identical(setequal(c(5, 3, 5), c(0, 2, 1, 3, 6)), base::setequal(c(5, 3, 5), c(0, 2, 1, 3, 6)))
-  expect_false(setequal(as.integer64(c(5, 3, 5)), as.integer64(c(0, 2, 1, 3, 6))))
-  expect_true(setequal(as.integer64(c(5, 3, 5)), as.integer64(c(3, 3, 3, 3, 5))))
-  
+
+  expect_identical(
+    setequal(c(5, 3, 5), c(0, 2, 1, 3, 6)),
+    base::setequal(c(5, 3, 5), c(0, 2, 1, 3, 6))
+  )
+  expect_false(setequal(
+    as.integer64(c(5, 3, 5)),
+    as.integer64(c(0, 2, 1, 3, 6))
+  ))
+  expect_true(setequal(
+    as.integer64(c(5, 3, 5)),
+    as.integer64(c(3, 3, 3, 3, 5))
+  ))
+
   expect_true(setequal(NA, NA_integer_))
   expect_true(setequal(NA_integer_, NA))
   expect_true(setequal(NA, NA_integer64_))
   expect_true(setequal(NA_integer64_, NA))
-  
+
 })
 
 with_parameters_test_that(
@@ -242,15 +253,9 @@ with_parameters_test_that(
     expected_result_y_x = base::is.element(y, x)
     y = as(y, type_y)
 
-    expect_identical(
-      is.element(x, y),
-      expected_result_x_y
-    )
-    expect_identical(
-      is.element(y, x),
-      expected_result_y_x
-    )
-  }, 
+    expect_identical(is.element(x, y), expected_result_x_y)
+    expect_identical(is.element(y, x), expected_result_y_x)
+  },
   .cases=expand.grid(
     x=I(list(NULL, c(1:7, 2L, 11L))),
     type_x=c(
@@ -263,7 +268,7 @@ with_parameters_test_that(
 )
 
 test_that("is.element works with special logic for double", {
-  
+
   expect_identical(is.element(1.5:7, 5:10), base::is.element(1.5:7, 5:10))
   expect_identical(is.element(1.5:7, as.integer64(6:1)), rep(TRUE, 6))
   expect_identical(is.element(as.integer64(6:1), 1.5:7), rep(TRUE, 6))
@@ -271,14 +276,23 @@ test_that("is.element works with special logic for double", {
 })
 
 test_that("is.element works (additional cases)", {
-  
-  expect_identical(is.element(c(5, 3, 5), c(0, 2, 1, 3, 6)), base::is.element(c(5, 3, 5), c(0, 2, 1, 3, 6)))
-  expect_identical(is.element(as.integer64(c(5, 3, 5)), as.integer64(c(0, 2, 1, 3, 6))), c(FALSE, TRUE, FALSE))
-  expect_identical(is.element(as.integer64(c(5, 3, 5)), as.integer64(c(3, 3, 3, 3, 5))), rep(TRUE, 3))
-  
+
+  expect_identical(
+    is.element(c(5, 3, 5), c(0, 2, 1, 3, 6)),
+    base::is.element(c(5, 3, 5), c(0, 2, 1, 3, 6))
+  )
+  expect_identical(
+    is.element(as.integer64(c(5, 3, 5)), as.integer64(c(0, 2, 1, 3, 6))),
+    c(FALSE, TRUE, FALSE)
+  )
+  expect_identical(
+    is.element(as.integer64(c(5, 3, 5)), as.integer64(c(3, 3, 3, 3, 5))),
+    rep(TRUE, 3)
+  )
+
   expect_true(is.element(NA, NA_integer_))
   expect_true(is.element(NA_integer_, NA))
   expect_true(is.element(NA, NA_integer64_))
   expect_true(is.element(NA_integer64_, NA))
-  
+
 })
