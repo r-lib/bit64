af535ffd76c1c1520d69439eca47425c0047b2fe
Michael Chirico <chiricom@google.com>
2026-05-22 23:34:55 -0700

    Fix runif64(replace=FALSE) to guarantee uniqueness (#338)
    * Fix runif64(replace=FALSE) to guarantee uniqueness
    
    * robustness around trying huge a:b

--- R/hash64.R
+++ R/hash64.R
@@ -389,8 +389,8 @@ runif64 <- function(n, min=lim.integer64()[1L], max=lim.integer64()[2L], replace
     d <- max - min + 1L
     if (!is.na(d) && N > d)
       stop("cannot take a sample larger than the population when 'replace = FALSE'")
-    if (!is.na(d) && n > d  / (2.0*log(n, 64.0))) {
-      ret <- .Call(C_runif_integer64, as.integer(d), as.integer64(min), as.integer64(max))
+    if (!is.na(d) && d <= .Machine$integer.max && n > d  / (2.0*log(n, 64.0))) {
+      ret <- .Call(C_seq_integer64, min, as.integer64(1L), double(as.integer(d)))
       oldClass(ret) <- "integer64"
       ret <- sample(ret, n, FALSE)
     } else {
