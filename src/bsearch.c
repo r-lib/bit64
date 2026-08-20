8606b4cc0ef1eb3e99c138cd7fc03bd56f3245a3
chiricom <chiricom@google.com>
2026-08-20 19:03:35 +0000

    Replace bsearch macros with static inline implementations

--- src/bsearch.c
+++ src/bsearch.c
@@ -8,19 +8,3 @@
 */
 
 #include "bsearch.h"
-
-IndexT integer64_bsearch_asc_EQ(ValueT *data, IndexT l, IndexT r, ValueT value){
-  INTEGER64_BSEARCH_ASC_EQ(data, l, r, value, return )
-}
-
-IndexT integer64_lsearch_asc_GE(ValueT *data, IndexT l, IndexT r, ValueT value){
-  INTEGER64_LSEARCH_ASC_GE(data, l, r, value, return )
-}
-
-IndexT integer64_bosearch_asc_EQ(ValueT *data, IndexT *index, IndexT l, IndexT r, ValueT value){
-  INTEGER64_BOSEARCH_ASC_EQ(data, index, l, r, value, return)
-}
-
-IndexT integer64_losearch_asc_GE(ValueT *data, IndexT *index, IndexT l, IndexT r, ValueT value){
-  INTEGER64_LOSEARCH_ASC_GE(data, index, l, r, value, return)
-}
